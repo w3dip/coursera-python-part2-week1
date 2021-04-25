94008cde3b459007b68816232a58990f8d31aa75
Павлов Иван Иванович <w3dipmail@gmail.com>
2021-04-25 14:34:57 +0300

    init commit

---
+++ factorize.py
@@ -0,0 +1,69 @@
+import unittest
+from contracts import contract
+
+
+@contract
+def factorize(x):
+    """
+    Factorize positive integer and return its factors.
+    :type x: int,>=0
+    :rtype: tuple[N],N>0
+    """
+    pass
+
+
+class TestFactorize(unittest.TestCase):
+
+    def test_wrong_types_raise_exception(self):
+        cases = [
+            'string', 1.5
+        ]
+        for x in cases:
+            with self.subTest(case=x):
+                self.assertRaises(TypeError, factorize, x)
+
+    def test_negative(self):
+        cases = [
+            -1, -10, -100
+        ]
+        for x in cases:
+            with self.subTest(case=x):
+                self.assertRaises(ValueError, factorize, x)
+
+    def test_zero_and_one_cases(self):
+        cases = [
+            (0, (0,)), (1, (1,))
+        ]
+        for x in cases:
+            with self.subTest(case=x):
+                self.assertTupleEqual(factorize(x[0]), x[1])
+
+    def test_simple_numbers(self):
+        cases = [
+            (3, (3,)), (13, (13,)), (29, (29,))
+        ]
+        for x in cases:
+            with self.subTest(case=x):
+                self.assertTupleEqual(factorize(x[0]), x[1])
+
+    def test_two_simple_multipliers(self):
+        cases = [
+            (6, (2, 3)), (26, (2, 13)), (121, (11, 11))
+        ]
+        for x in cases:
+            with self.subTest(case=x):
+                self.assertTupleEqual(factorize(x[0]), x[1])
+
+    def test_many_multipliers(self):
+        cases = [
+            (1001, (7, 11, 13)), (9699690, (2, 3, 5, 7, 11, 13, 17, 19))
+        ]
+        for x in cases:
+            with self.subTest(case=x):
+                self.assertTupleEqual(factorize(x[0]), x[1])
+
+# factorize(-1)
+
+if __name__ == "__main__":
+    unittest.main()
+    # factorize(-1)
